3d73009c18a546371ada10c5a06454da67f2a727
descent1511 <natibatian@gmail.com>
2024-05-20 18:04:06 +0300

    feat: add router for project

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomePage.vue"),  
+    },
+
+    {
+      path: '/profile/:id',
+      name: "profile",
+      component: () => import("../views/ProfilePage.vue"),
+      props: true,
+    },
+  ],
+});
+
+
+export default router;
