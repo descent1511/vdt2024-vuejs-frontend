a5501e928b62ce085ff1e885e11ad3b377988a4f
descent1511 <natibatian@gmail.com>
2024-05-20 17:53:02 +0300

    feat: add all api for user

--- src/api/userService.js
+++ src/api/userService.js
@@ -9,3 +9,20 @@ export const createUser = (userData) => {
   return axios.post('/v1/users', userData);
 };
 
+export const getUserById = (userId) => {
+  return axios.get(`/v1/users/${userId}`);
+};
+
+export const updateUser = (userId, userData) => {
+  return axios.put(`/v1/users/${userId}`, userData);
+};
+
+export const deleteUser = (userId) => {
+  return axios.delete(`/v1/users/${userId}`);
+};
+
+export const searchUserByName = (searchQuery) => {
+  return axios.get('/v1/search', {
+    params: { search: searchQuery }
+  });
+};
